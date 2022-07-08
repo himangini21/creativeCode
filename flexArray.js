92804ef9e6aa46ef21cce9381fd4fc5857a25e62
himangini21 <108763635+himangini21@users.noreply.github.com>
2022-07-08 15:14:11 -0500

    Add files via upload

---
+++ flexArray.js
@@ -0,0 +1,34 @@
+
+let x = []
+let y = []
+
+let furby
+
+function preload () {
+  furby = loadImage ('furby.png')
+}
+
+function setup () {
+  createCanvas (windowWidth, windowHeight)
+  imageMode (CENTER)
+
+}
+
+function draw () {
+  background (0)
+  for (let i = 0; i < x.length; i++) {
+    image (furby, x [i], y [i], 60, 60)
+    if (dist (mouseX, mouseY, x[i], y[i]) < 30) {
+      x.splice (i, 1)
+      y.splice (i, 1)
+    }
+  }
+}
+
+function keyPressed () {
+  if (key === 'a') {
+    x.push (random(windowWidth))
+    y.push (random(windowHeight))
+  }
+
+}
